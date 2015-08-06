3a7e47869d51bde8882f5a6ffd478d53dafe95ec
Jhow <jhowliu@gmail.com>
2015-08-06 20:44:03 +0800

    add method of like counting

--- tool/crawler.py
+++ tool/crawler.py
@@ -17,7 +17,6 @@ class Crawler:
         self.page_id = page_id
         self.token = token
         self.all_posts = []
-        self.all_like_counts = []
         self.delay = 300
 
     def get_all_posts(self, args={'fields':'id,message,link', 'until':''}):
@@ -25,8 +24,8 @@ class Crawler:
         try:
             posts = self.graph.get_object(self.page_id + "/posts", **args)
         except:
-            print "Time to delay, Saving.", len(self.all_posts)
-            self.save(datetime.now().strftime("%Y-%m-%d %H:%M:%S.json"))
+            print "Time to delay, Saving. (5 Mins)", len(self.all_posts)
+            self.save(datetime.now().strftime("Post_%Y-%m-%d %H:%M:%S.json"))
             time.sleep(self.delay)
             posts = self.graph.get_object(self.page_id + "/posts", **args)
 
@@ -40,11 +39,29 @@ class Crawler:
         # return the next page id
         return parser.get_nextpage_id(posts['paging']['next'])
 
-    def get_all_like_counts(self, args={'limit':10000, 'after':''}):
-
+    def get_like_counts(self, args={'limit':10000, 'after':''}):
+        like_counts = []
         for post in self.all_posts:
-            try:
-                likes = self.graph.get_object(post['id'] + )
+            like_count = 0
+            print post['id']
+            while True:
+                try:
+                    likes = self.graph.get_object(post['id'] + '/likes', **args)
+                    like_count = like_count + len(likes['data'])
+                except:
+                    print "Time to delay. (5 Mins)"
+                    time.sleep(self.delay)
+                    likes = self.graph.get_object(post['id'] + '/likes', **args)
+                # get the id of next page
+                try:
+                    args['after'] = likes['paging']['cursors']['after']
+                except:
+                    print "No any next pages"
+                    like_counts.append(like_count)
+                    args['after'] = ''
+                    break
+
+        return like_counts
 
     def save(self, filename):
         out = open(filename, 'w')
